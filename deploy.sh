8351b31147ce6f11e72ee32f0d3842d8f54170ba
Randy Butts <randybuttsii@gmail.com>
2022-02-07 00:51:23 -0500

    Moved cred setting to script

--- deploy.sh
+++ deploy.sh
@@ -8,6 +8,7 @@ cd dist
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 git init
+git config user.name "Github Actions" && git config user.email "cicd@privatebutts.dev"
 git add -A
 git commit -m 'deploy'
 git push -f git@github.com:PrivateButts/wordle-hints.git master:gh-pages
